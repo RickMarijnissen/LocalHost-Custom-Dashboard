a7786a5e0d3fcf46bd2350e8389f8fc541b10598
RickMarijnissen <89875@roc-teraa.nl>
2025-02-19 13:10:10 +0100

    Update open_folder.php

--- dashboard/inc/open_folder.php
+++ dashboard/inc/open_folder.php
@@ -3,3 +3,7 @@ $folderPath = 'C:\laragon';
 
 // Open the folder in Windows Explorer
 shell_exec('explorer ' . escapeshellarg($folderPath));
+
+// Redirect back to localhost
+header("Location: http://localhost/");
+exit;
